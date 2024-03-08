613c1a32a9c4221a6e4495a61692317132496505
arisihiteeee <162563005+arisihiteeee@users.noreply.github.com>
2024-03-08 11:08:23 +0700

    Add files via upload

---
+++ Menghitungluaspersegi.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package menghitungluaspersegi;
+
+/**
+ *
+ * @author LAPTOP
+ */
+public class Menghitungluaspersegi {
+
+    /**
+     * @param args the commannts
+     */
+    public  void setPanjang(int panjang)  {
+        // TODO code application logic here
+       
+    }
+    
+}
